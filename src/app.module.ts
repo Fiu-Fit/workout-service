80f4b1ca449ff0fbdfd11e6c47d2dcdf6228d74e
Rocío <rplatini@fi.uba.ar>
2023-04-19 17:11:26 -0300

    Register module on app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { WorkoutsModule } from './modules/workout/workouts.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.MONGODB_URL || '')],
+  imports: [
+    WorkoutsModule,
+    MongooseModule.forRoot(process.env.MONGODB_URL || ''),
+  ],
+  controllers: [],
 })
 export class AppModule {}
