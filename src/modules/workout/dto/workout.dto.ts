e6170691cf306cd45f4938485faf86c210ee1bc8
Rocío <rplatini@fi.uba.ar>
2023-04-29 16:23:53 -0300

    Update proto and WorkoutDto

--- src/modules/workout/dto/workout.dto.ts
+++ src/modules/workout/dto/workout.dto.ts
@@ -1,35 +1,19 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
-
-export class ExerciseInfo {
-  @IsString()
-  exerciseId: string;
-
-  @IsNumber()
-  repetitions?: number;
-
-  @IsNumber()
-  duration?: number;
-}
+import { WorkoutExercise } from '../interfaces/workout.pb';
 
 export class WorkoutDto {
-  @IsString()
-  @IsNotEmpty()
   name: string;
 
-  @IsString()
   description?: string;
 
-  @IsNumber()
   duration: number;
 
-  @IsNumber()
   difficulty: number;
 
-  @IsString()
   category?: string;
 
-  exercises: ExerciseInfo[];
+  exercises: WorkoutExercise[];
 
-  @IsNumber()
+  athleteIds: number[] = [];
+
   authorId: number;
 }
--- src/modules/workout/interfaces/workout.pb.ts
+++ src/modules/workout/interfaces/workout.pb.ts
@@ -1,39 +1,46 @@
 /* eslint-disable */
 import { GrpcMethod, GrpcStreamMethod } from '@nestjs/microservices';
 import { Observable } from 'rxjs';
+import { WorkoutDto } from '../dto/workout.dto';
 
 export const protobufPackage = 'workout';
+
+export enum Units {
+  SECONDS = 0,
+  MINUTES = 1,
+  HOURS = 2,
+  REPETITIONS = 3,
+  METERS = 4,
+  KILOMETERS = 5,
+  UNRECOGNIZED = -1,
+}
 
 export interface WorkoutId {
   id: string;
 }
 
 export interface Workout {
+  id: string;
   name: string;
   description: string;
   duration: number;
   difficulty: number;
   category: string;
-  exercises: ExerciseInfo[];
+  exercises: WorkoutExercise[];
   athleteIds: number[];
   authorId: number;
 }
 
-export interface ExerciseInfo {
+export interface WorkoutExercise {
   exerciseId: string;
   repetitions: number;
-  duration: number;
+  units: Units;
 }
 
 export interface Empty {}
 
-export interface WorkoutList {
+export interface Workouts {
   workouts: Workout[];
-}
-
-export interface WorkoutPutRequest {
-  id: string;
-  workout: Workout;
 }
 
 export interface WorkoutName {
@@ -51,25 +58,25 @@ export interface ExerciseId {
 export const WORKOUT_PACKAGE_NAME = 'workout';
 
 export interface WorkoutServiceClient {
-  create(request: Workout): Observable<Workout>;
+  create(request: WorkoutDto): Observable<Workout>;
 
   findById(request: WorkoutId): Observable<Workout>;
 
-  findByCategory(request: WorkoutCategory): Observable<WorkoutList>;
+  findByCategory(request: WorkoutCategory): Observable<Workouts>;
 
   findByName(request: WorkoutName): Observable<Workout>;
 
-  findByExerciseId(request: ExerciseId): Observable<WorkoutList>;
+  findByExerciseId(request: ExerciseId): Observable<Workouts>;
 
-  findAll(request: Empty): Observable<WorkoutList>;
+  findAll(request: Empty): Observable<Workouts>;
 
-  put(request: WorkoutPutRequest): Observable<Workout>;
+  put(request: Workout): Observable<Workout>;
 
   deleteById(request: WorkoutId): Observable<Workout>;
 }
 
 export interface WorkoutServiceController {
-  create(request: Workout): Promise<Workout> | Observable<Workout> | Workout;
+  create(request: WorkoutDto): Promise<Workout> | Observable<Workout> | Workout;
 
   findById(
     request: WorkoutId
@@ -77,7 +84,7 @@ export interface WorkoutServiceController {
 
   findByCategory(
     request: WorkoutCategory
-  ): Promise<WorkoutList> | Observable<WorkoutList> | WorkoutList;
+  ): Promise<Workouts> | Observable<Workouts> | Workouts;
 
   findByName(
     request: WorkoutName
@@ -85,15 +92,11 @@ export interface WorkoutServiceController {
 
   findByExerciseId(
     request: ExerciseId
-  ): Promise<WorkoutList> | Observable<WorkoutList> | WorkoutList;
+  ): Promise<Workouts> | Observable<Workouts> | Workouts;
 
-  findAll(
-    request: Empty
-  ): Promise<WorkoutList> | Observable<WorkoutList> | WorkoutList;
+  findAll(request: Empty): Promise<Workouts> | Observable<Workouts> | Workouts;
 
-  put(
-    request: WorkoutPutRequest
-  ): Promise<Workout> | Observable<Workout> | Workout;
+  put(request: Workout): Promise<Workout> | Observable<Workout> | Workout;
 
   deleteById(
     request: WorkoutId
