6cb3a3fe4988a632a4fcf39b2940cfeabe0eff24
ianshih <ianshih2003@gmail.com>
2023-05-03 15:17:25 -0300

    Rename headers

--- src/utils/api-key-guard.ts
+++ src/utils/api-key-guard.ts
@@ -8,7 +8,7 @@ export class ApiKeyGuard implements CanActivate {
 
   canActivate(context: ExecutionContext): boolean {
     const req = context.switchToHttp().getRequest();
-    const key = req.headers['X-API-KEY'] ?? req.query.api_key; // checks the header, moves to query if null
+    const key = req.headers['Api-Key'] ?? req.query.api_key; // checks the header, moves to query if null
     return this.validateApiKey(key);
   }
 }
