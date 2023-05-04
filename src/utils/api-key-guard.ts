5bef8f77d39d520d03b8104a00bc2b0f1d92ea56
ianshih <ianshih2003@gmail.com>
2023-05-04 15:38:46 -0300

    Fix

--- src/utils/api-key-guard.ts
+++ src/utils/api-key-guard.ts
@@ -8,7 +8,7 @@ export class ApiKeyGuard implements CanActivate {
 
   canActivate(context: ExecutionContext): boolean {
     const req = context.switchToHttp().getRequest();
-    const key = req.headers['Api-Key'] ?? req.query.apiKey; // checks the header, moves to query if null
+    const key = req.headers['api-key'] ?? req.query.apiKey; // checks the header, moves to query if null
     return this.validateApiKey(key);
   }
 }
